350c31c4ec733eb7525f3379300143465be426a1
sierpien <stepnikagata@gmail.com>
2021-06-13 17:06:54 +0200

    Damian changes until sunday

---
+++ blocks/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AWSA</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_bloginfo('template_directory')?>/assets/img/favicon16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_bloginfo('template_directory')?>/assets/img/favicon32.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="<?php echo get_bloginfo('template_directory')?>/assets/img/favicon64.png">
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory')?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory')?>/css/style.css">
+</head>
---
+++ blocks/navigation.php
@@ -0,0 +1,7 @@
+<nav class="container-fluid navigation">
+    <div class="row g-0">
+        <a href="index.html" class="navigation__logo">
+            <img src="<?php echo get_bloginfo('template_directory')?>/assets/img/logo-color.svg" alt="AWSA logo" loading="eager">
+        </a>
+    </div>
+</nav>
